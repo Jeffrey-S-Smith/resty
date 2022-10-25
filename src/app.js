09da52d7a5d6fd2964e3e5755773b529d3da0847
Jeffrey Smith <jeffrey_s_smith@live.com>
2022-10-25 12:16:09 -0700

    updated phase 1

--- src/app.js
+++ src/app.js
@@ -1,94 +1,87 @@
-import React from 'react';
-
-import './app.scss';
-
-// Let's talk about using index.js and some other name in the component folder
-// There's pros and cons for each way of doing this ...
-import Header from './components/header';
-import Footer from './components/footer';
-import Form from './components/form';
-import Results from './components/results';
-
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      requestParams: {},
-    };
-  }
-
-  callApi = (requestParams) => {
-    // mock output
-    const data = {
-      count: 2,
-      results: [
-        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-      ],
-    };
-    this.setState({data, requestParams});
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <div>Request Method: {this.state.requestParams.method}</div>
-        <div>URL: {this.state.requestParams.url}</div>
-        <Form handleApiCall={this.callApi} />
-        <Results data={this.state.data} />
-        <Footer />
-      </React.Fragment>
-    );
-  }
-}
-
-export default App;
-
-// import React, { useState } from 'react';
-
+// import React from 'react';
 // import './app.scss';
 
-
+// // Let's talk about using index.js and some other name in the component folder
+// // There's pros and cons for each way of doing this ...
 // import Header from './components/header';
 // import Footer from './components/footer';
 // import Form from './components/form';
 // import Results from './components/results';
 
-// const [data, setData] = useState(null);
-// const [requestParams, setRequestParams] = useState({});
-
 // class App extends React.Component {
 
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: null,
+//       requestParams: {},
+//     };
+//   }
 
+//   callApi = (requestParams) => {
+//     // mock output
+//     const data = {
+//       count: 2,
+//       results: [
+//         {name: 'fake thing 1', url: 'http://fakethings.com/1'},
+//         {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+//       ],
+//     };
+//     this.setState({data, requestParams});
+//   }
 
-// callApi = (requestParams) => {
-
-//   const data = {
-//     count: 2,
-//     results: [
-//       {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-//       {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-//     ],
-//   };
-//   setData(data);
-//   setRequestParams(requestParams);
-// }
-
-// render() {
-//   return (
-//     <React.Fragment>
-//       <Header />
-//       <div>Request Method: {requestParams.method}</div>
-//       <div>URL: {requestParams.url}</div>
-//       <Form handleApiCall={callApi} />
-//       <Results data={data} />
-//       <Footer />
-//     </React.Fragment>
-//   );
-// }
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <Header />
+//         <div>Request Method: {this.state.requestParams.method}</div>
+//         <div>URL: {this.state.requestParams.url}</div>
+//         <Form handleApiCall={this.callApi} />
+//         <Results data={this.state.data} />
+//         <Footer />
+//       </React.Fragment>
+//     );
+//   }
 // }
 
 // export default App;
+
+import React, { useState } from 'react';
+import './app.scss';
+import Header from './components/header';
+import Footer from './components/footer';
+import Form from './components/form';
+import Results from './components/results';
+
+const [data, setData] = useState(null);
+const [requestParams, setRequestParams] = useState({});
+
+const App = () => {
+
+callApi = (requestParams) => {
+
+  const data = {
+    count: 2,
+    results: [
+      {name: 'fake thing 1', url: 'http://fakethings.com/1'},
+      {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+    ],
+  };
+  setData(data);
+  setRequestParams(requestParams);
+}
+
+  return (
+    <React.Fragment>
+      <Header />
+      <div>Request Method: {requestParams.method}</div>
+      <div>URL: {requestParams.url}</div>
+      <Form handleApiCall={callApi} />
+      <Results data={data} />
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+
+export default App;
--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,85 +1,84 @@
-import React from 'react';
-
-import './form.scss';
-
-class Form extends React.Component {
-
-  handleSubmit = e => {
-    e.preventDefault();
-    const formData = {
-      method:'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
-    };
-    this.props.handleApiCall(formData);
-  }
-
-  render() {
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <label >
-            <span>URL: </span>
-            <input name='url' type='text' />
-            <button type="submit">GO!</button>
-          </label>
-          <label className="methods">
-            <span id="get">GET</span>
-            <span id="post">POST</span>
-            <span id="put">PUT</span>
-            <span id="delete">DELETE</span>
-          </label>
-        </form>
-      </>
-    );
-  }
-}
-
-export default Form;
-
-// import React, { useState } from 'react';
+// import React from 'react';
 
 // import './form.scss';
 
-// const Form = (props) => {
+// class Form extends React.Component {
 
-//   let [method, setMethod] = useState('');
-//   let [url, setUrl] = useState('');
-
-//   const handleSubmit = e => {
+//   handleSubmit = e => {
 //     e.preventDefault();
 //     const formData = {
-//       method,
-//       url,
+//       method:'GET',
+//       url: 'https://pokeapi.co/api/v2/pokemon',
 //     };
-//     props.handleApiCall(formData);
+//     this.props.handleApiCall(formData);
 //   }
 
-//   const onChange = (e) => {
-//     let {name, value} = e.target;
-//     if (name === 'url') setUrl(value);
-//     if (name === 'get' && checked) setMethod(value);
-//     if (name === 'post' && checked) setMethod(value);
-//     if (name === 'put' && checked) setMethod(value);
-//     if (name === 'delete' && checked) setMethod(value);
+//   render() {
+//     return (
+//       <>
+//         <form onSubmit={this.handleSubmit}>
+//           <label >
+//             <span>URL: </span>
+//             <input name='url' type='text' />
+//             <button type="submit">GO!</button>
+//           </label>
+//           <label className="methods">
+//             <span id="get">GET</span>
+//             <span id="post">POST</span>
+//             <span id="put">PUT</span>
+//             <span id="delete">DELETE</span>
+//           </label>
+//         </form>
+//       </>
+//     );
 //   }
-
-//   return (
-//     <>
-//       <form onSubmit={handleSubmit}>
-//         <label >
-//           <span>URL: </span>
-//           <input name='url' type='text' onChange={onChange}/>
-//         </label>
-//         <label className="methods">
-//           <input type="radio" name="get" id="get" onChange={onChange}>GET</input>
-//           <input type="radio" name="post" id="post" onChange={onChange}>POST</input>
-//           <input type="radio" name="put" id="put" onChange={onChange}>PUT</input>
-//           <input type="radio" name="delete" id="delete" onChange={onChange}>DELETE</input>
-//         </label>
-//         <button type="submit">GO!</button>
-//       </form>
-//     </>
-//   )
 // }
 
 // export default Form;
+
+import React, { useState } from 'react';
+import './form.scss';
+
+const Form = (props) => {
+
+  let [method, setMethod] = useState('');
+  let [url, setUrl] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const formData = {
+      method,
+      url,
+    };
+    props.handleApiCall(formData);
+  }
+
+  const onChange = (e) => {
+    let {name, value} = e.target;
+    if (name === 'url') setUrl(value);
+    if (name === 'get' && checked) setMethod(value);
+    if (name === 'post' && checked) setMethod(value);
+    if (name === 'put' && checked) setMethod(value);
+    if (name === 'delete' && checked) setMethod(value);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label >
+          <span>URL: </span>
+          <input name='url' type='text' onChange={onChange}/>
+        </label>
+        <label className="methods">
+          <input type="radio" name="get" id="get" onChange={onChange}>GET</input>
+          <input type="radio" name="post" id="post" onChange={onChange}>POST</input>
+          <input type="radio" name="put" id="put" onChange={onChange}>PUT</input>
+          <input type="radio" name="delete" id="delete" onChange={onChange}>DELETE</input>
+        </label>
+        <button type="submit">GO!</button>
+      </form>
+    </>
+  )
+}
+
+export default Form;
