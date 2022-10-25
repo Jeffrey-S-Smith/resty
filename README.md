bdeee92bcc58119b5e6b2987156c1ed4a57b1ab4
Jeffrey Smith <jeffrey_s_smith@live.com>
2022-10-24 19:34:57 -0700

    phase 1 update

--- README.md
+++ README.md
@@ -31,3 +31,6 @@ Students will be creating and styling a Counter application. Look for the follow
 - The first and most important part of the lab assignment is to draw a process diagram of the starter code and state as events happen. Once the students understand that, they can move on.
 
 ![UML](img/Lab26-UML.png)
+
+site: https://venerable-stardust-a6bdb2.netlify.app/
+
--- src/app.js
+++ src/app.js
@@ -46,3 +46,49 @@ class App extends React.Component {
 }
 
 export default App;
+
+// import React, { useState } from 'react';
+
+// import './app.scss';
+
+
+// import Header from './components/header';
+// import Footer from './components/footer';
+// import Form from './components/form';
+// import Results from './components/results';
+
+// const [data, setData] = useState(null);
+// const [requestParams, setRequestParams] = useState({});
+
+// class App extends React.Component {
+
+
+
+// callApi = (requestParams) => {
+
+//   const data = {
+//     count: 2,
+//     results: [
+//       {name: 'fake thing 1', url: 'http://fakethings.com/1'},
+//       {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+//     ],
+//   };
+//   setData(data);
+//   setRequestParams(requestParams);
+// }
+
+// render() {
+//   return (
+//     <React.Fragment>
+//       <Header />
+//       <div>Request Method: {requestParams.method}</div>
+//       <div>URL: {requestParams.url}</div>
+//       <Form handleApiCall={callApi} />
+//       <Results data={data} />
+//       <Footer />
+//     </React.Fragment>
+//   );
+// }
+// }
+
+// export default App;
