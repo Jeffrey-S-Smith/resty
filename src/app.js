9539eb3dcdfebbf15fe7539feda67f977623114e
Jeffrey Smith <jeffrey_s_smith@live.com>
2022-10-27 22:46:26 -0700

    phase 3 and phase 4

--- src/app.js
+++ src/app.js
@@ -1,27 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import './app.scss';
 import Header from './components/header';
 import Footer from './components/footer';
 import Form from './components/form';
 import Results from './components/results';
 
+const initialState = {
+  loading: false,
+  results: null,
+  history: [],
+  request: {},
+};
+
+const reducer = (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case 'begin':
+      return { ...state, request: payload, loading: true }
+    default:
+      return state;
+  }
+};
 
 
 const App = () => {
 
-  let [data, setData] = useState(null);
-  let [requestParams, setRequestParams] = useState({});
-  let [loading, setLoading] = useState(false);
-  
+  // let [data, setData] = useState(null);
+  // let [requestParams, setRequestParams] = useState({});
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const callApiReducer = async (searchQuery) => {
+    const action = {
+      type: "begin",
+      payload: searchQuery,
+    }
+    dispatch(action);
+  }
+
   const fetchData = (requestParams) => {
     console.log(requestParams);
     fetch(requestParams.url)
-                  .then((res) => res.json())
-                  .then((json) => {
-                      setData(json);
-                  })
-          
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json);
+      })
+
   }
+
+  useEffect(() => {
+    if (state.loading === true && state.request.method && state.request.url) {
+      fetchData(state.request);
+    }
+
+  }, [requestParams]);
 
   const callApi = async (requestParams) => {
 
@@ -33,18 +64,17 @@ const App = () => {
       ],
     };
     setData(data);
-    setLoading(false);
     await setRequestParams(requestParams);
     await fetchData(requestParams);
   }
 
-  
+
   return (
     <React.Fragment>
       <Header />
       <div>Request Method: {requestParams.method}</div>
       <div>URL: {requestParams.url}</div>
-      <Form loading={ loading } handleApiCall={callApi} />
+      <Form handleApiCall={callApiReducer} />
       <Results data={data} />
       <Footer />
     </React.Fragment>
